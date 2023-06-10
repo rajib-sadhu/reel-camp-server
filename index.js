315f1ad066a226125ab12c63b56342373fa71893
rajib-sadhu <rahulrajib.sadhu@gmail.com>
2023-06-10 14:38:09 +0530

    add users collection on it

--- index.js
+++ index.js
@@ -1,7 +1,109 @@
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const app = express();
-
+const cors = require('cors')
 const port = process.env.PORT || 5000;
+const jwt = require('jsonwebtoken');
+
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const verifyJwt = (req, res, next) => {
+  const authorization = req.headers.authorization;
+
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' })
+  }
+
+  // bearer token
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+    if (error) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
+
+var uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ac-kodbbia-shard-00-00.6iqnpnz.mongodb.net:27017,ac-kodbbia-shard-00-01.6iqnpnz.mongodb.net:27017,ac-kodbbia-shard-00-02.6iqnpnz.mongodb.net:27017/?ssl=true&replicaSet=atlas-12qn8w-shard-0&authSource=admin&retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    client.connect();
+
+    // Collections
+    const classesCollection = client.db('reelCamp').collection('classes');
+    const usersCollection = client.db('reelCamp').collection('users');
+
+    // Warning : use verifyJwt before using verifyAdmin
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+
+      if (user?.role !== 'admin') {
+        return res.status(403).send({ error: true, message: 'forbidden access' })
+      }
+      next();
+    }
+
+    // Check JWT
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ token });
+    })
+
+    // Users API
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'User already exists' })
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+
+    // Classes API
+    app.get('/classes', async (req, res) => {
+
+      const result = await classesCollection.find().toArray();
+      res.send(result)
+
+    })
+
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 
 
 
@@ -9,12 +111,9 @@ const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
 
-    res.send('Reel Camp Server Running');
+  res.send('Reel Camp Server Running');
 });
 
-
-
-
 app.listen(port, () => {
-    console.log('Localhost Port:', port);
+  console.log('Localhost Port:', port);
 })
